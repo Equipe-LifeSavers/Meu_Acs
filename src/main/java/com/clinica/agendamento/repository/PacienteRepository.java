ba28109e8db90dc8866d7994e6d145d518b8dce9
Viniciusvass <viniciusvasconcelossantos13@gmail.com>
2026-06-15 12:50:03 -0300

    Removendo entidades do sistema de agendamento.

--- src/main/java/com/clinica/agendamento/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clinica.agendamento.repository;
-
-import com.clinica.agendamento.model.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-}

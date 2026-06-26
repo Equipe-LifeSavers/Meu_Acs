4aa3ff513e6595ef530db4ff66f634f4c2486fb1
Toin-Paulo <antoniopaulosilva345@gmail.com>
2026-06-25 21:47:08 -0300

    Implementação do CRUD Residencia

--- src/main/java/com/clinica/agendamento/model/Residencia.java
+++ src/main/java/com/clinica/agendamento/model/Residencia.java
@@ -1,5 +1,25 @@
 package com.clinica.agendamento.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "residencias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Residencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "regiao_id", nullable = false)
+    private Regiao regiao;
 
 }

2e5845513a9513a8cfa092d4fca673dc78fe5993
Paulo Eduardo <peduardos22@outlook.com.br>
2026-07-02 20:22:23 -0300

    integrar autenticação mockada ao logincontroller

--- frontend/lib/features/auth/login/login_controller.dart
+++ frontend/lib/features/auth/login/login_controller.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import '../../../data/repositories/auth_repository.dart';
 
 class LoginController {
   final cpfController = TextEditingController();
   final passwordController = TextEditingController();
-
   final formKey = GlobalKey<FormState>();
+  final AuthRepository _authRepository = AuthRepository();
+
+  Future<bool> login() async {
+    final usuario = await _authRepository.login(
+      cpf: cpfController.text.trim(),
+      senha: passwordController.text.trim(),
+    );
+    return usuario != null;
+  }
 
   void dispose() {
     cpfController.dispose();

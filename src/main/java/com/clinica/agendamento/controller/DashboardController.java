f5526e0015af486c099c7d21e5bac0036a6fc2c0
Toin-Paulo <antoniopaulosilva345@gmail.com>
2026-07-21 13:57:26 -0300

    feat(dashboard): gerencial

--- src/main/java/com/clinica/agendamento/controller/DashboardController.java
+++ src/main/java/com/clinica/agendamento/controller/DashboardController.java
@@ -6,10 +6,10 @@ import com.clinica.agendamento.repository.AcsRepository;
 import com.clinica.agendamento.repository.FamiliaRepository;
 import com.clinica.agendamento.repository.MoradorRepository;
 import com.clinica.agendamento.repository.RegiaoRepository;
+import com.clinica.agendamento.repository.ResidenciaRepository;
+import com.clinica.agendamento.repository.UbsRepository;
 import com.clinica.agendamento.repository.VisitaRepository;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +20,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DashboardController {
 
+    private final UbsRepository ubsRepository;
     private final RegiaoRepository regiaoRepository;
     private final AcsRepository acsRepository;
+    private final ResidenciaRepository residenciaRepository;
     private final FamiliaRepository familiaRepository;
     private final MoradorRepository moradorRepository;
     private final VisitaRepository visitaRepository;
@@ -32,9 +34,13 @@ public class DashboardController {
 
         return new DashboardResponse(
 
+                ubsRepository.count(),
+
                 regiaoRepository.count(),
 
                 acsRepository.count(),
+
+                residenciaRepository.count(),
 
                 familiaRepository.count(),
 
@@ -47,7 +53,6 @@ public class DashboardController {
                 visitaRepository.countByVisitaRealizadaFalse()
 
         );
-
     }
 
     @GetMapping("/demandas")
@@ -58,14 +63,14 @@ public class DashboardController {
 
                 new IndicadorDemandaResponse(
                         "Visitas realizadas",
-                        visitaRepository.countByVisitaRealizadaTrue()),
+                        visitaRepository.countByVisitaRealizadaTrue()
+                ),
 
                 new IndicadorDemandaResponse(
                         "Visitas pendentes",
-                        visitaRepository.countByVisitaRealizadaFalse())
+                        visitaRepository.countByVisitaRealizadaFalse()
+                )
 
         );
-
     }
-
 }
--- src/main/java/com/clinica/agendamento/dto/DashboardResponse.java
+++ src/main/java/com/clinica/agendamento/dto/DashboardResponse.java
@@ -2,9 +2,13 @@ package com.clinica.agendamento.dto;
 
 public record DashboardResponse(
 
+        Long totalUbs,
+
         Long totalRegioes,
 
         Long totalAcs,
+
+        Long totalResidencias,
 
         Long totalFamilias,
 

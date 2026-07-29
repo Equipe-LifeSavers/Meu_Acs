744daacafcfbfcc6c712e9cb470713abba606823
Paulo Eduardo <peduardos22@outlook.com.br>
2026-07-29 00:45:26 -0300

    feat: Adicionar  modificador de localização padrão

--- frontend/lib/app/app.dart
+++ frontend/lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../core/theme/app_theme.dart';
 import 'app_routes.dart';
 import '../core/constants/app_strings.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 
 class MeuACSApp extends StatelessWidget {
@@ -10,6 +11,15 @@ class MeuACSApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      locale: const Locale('pt', 'BR'),
+      supportedLocales: const [Locale('pt', 'BR')],
+
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
       debugShowCheckedModeBanner: false,
       title: AppStrings.appName,
       theme: AppTheme.lightTheme,
